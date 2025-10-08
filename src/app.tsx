0425782c1fc544fe3aab20d3248cfbdfb5fac2ec
Luis Ramos <luisviegasr2+github@gmail.com>
2025-10-08 21:57:48 +0100

    .

--- src/app.tsx
+++ src/app.tsx
@@ -2,17 +2,18 @@ import { MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
-import "./app.css";
+import "~/styles/layout.scss";
 
 export default function App() {
   return (
     <Router
       root={props => (
         <MetaProvider>
-          <Title>SolidStart - Basic</Title>
-          <a href="/">Index</a>
-          <a href="/about">About</a>
-          <Suspense>{props.children}</Suspense>
+          <Suspense>
+            <div class="layout">
+            {props.children}
+            </div>
+            </Suspense>
         </MetaProvider>
       )}
     >
