a7c845715cdc81392475fa8fd5bc73b3079266f4
Luis Ramos <luisviegasr2+github@gmail.com>
2025-10-28 16:44:50 +0000

    A button with some action

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,29 @@
 import { Title } from "@solidjs/meta";
+import { createResource } from "solid-js";
+import "~/styles/landing.scss";
 
+
+const fetchData = async () => {
+  const response = await fetch("https://api.metrolisboa.pt:8243/estadoServicoML/1.0.1/estadoLinha/todos", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`,
+      accept: "application/json",
+    },
+  });
+  return response.json();
+}
 
 export default function Home() {
   return (
-    <main>
+    <>
       <Title>Metro Lisboa</Title>
-      
-    </main>
+      <h1>Welcome to Metro Lisboa</h1>
+      <h2>Get data:</h2>
+      <button onClick={async () => {
+        const data = await fetchData();
+        console.log(data);
+      }}>Fetch Metro Data</button>
+    </>
   );
 }
